89b6fbefa0f439c1f00e9c39ddf2096dfc369693
mohsen-alizadeh <dev.medsa@gmail.com>
2021-04-12 10:26:41 +0430

    install and configure tailwind css for vue component and laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    <body>
+        <div class="bg-gray-400 gird grid-cols-2 h-screen">
+            <p class="text-red-400 text-center text-5xl">
+                Hello laravel
+            </p>
+        </div>
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
